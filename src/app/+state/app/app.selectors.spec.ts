be1dba16b0406421f9887d344ec9187c3459aa60
camposmichel <michelcsilva@live.com>
2022-06-18 22:22:27 -0300

    test: adjusts

--- src/app/+state/app/app.selectors.spec.ts
+++ src/app/+state/app/app.selectors.spec.ts
@@ -1,12 +1,19 @@
+import { TabType } from 'src/app/core/models/app.enum';
 import * as fromApp from './app.reducer';
+import { State } from './app.reducer';
 import { selectAppState } from './app.selectors';
 
 describe('App Selectors', () => {
   it('should select the feature state', () => {
+    const initialState: State = {
+      loading: false,
+      tabType: TabType.CHARACTER,
+    };
+
     const result = selectAppState({
       [fromApp.appFeatureKey]: {}
     });
 
-    expect(result).toEqual({});
+    expect(result).toEqual(initialState);
   });
 });
